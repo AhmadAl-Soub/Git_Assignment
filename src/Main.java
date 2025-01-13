eb7c768cb12bca6d859b8b8be0d9b9bf538b246a
AhmadAlSoub <ahmadsoub.ce13@gmail.com>
2025-01-13 08:24:06 +0300

    Create a Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 public class Main {
+    //Mid-Level Developer
+    //Build The Multiply Function here
+    //Build The Divide Function here
+    //Build The Module Function here
+
+    //Junior Developer
+    //Build The Addition Function here
+    //Build The Subtraction Function here
+
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Simple Calculator - Version V0.0");
+        System.out.print("Please Enter The Calculation Type: ");
+        char type = sc.next().charAt(0);
+        System.out.print("Please Enter The First Number: ");
+        double number1 = sc.nextDouble();
+        System.out.print("Please Enter The Second Number: ");
+        double number2 = sc.nextDouble();
+
 
     }
 }
